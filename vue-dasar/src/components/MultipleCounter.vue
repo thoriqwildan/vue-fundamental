0ac7d14d5a4e444d4211cebfb12510c40dc77392
thoriqwildan <wildanthoriq14@gmail.com>
2025-06-17 15:26:42 +0700

    learn Component

---
+++ vue-dasar/src/components/MultipleCounter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Counter from "./Counter.vue";
+</script>
+
+<template>
+  <h1>Counter Thoriq</h1>
+  <Counter />
+
+  <h1>Counter Tirta</h1>
+  <Counter />
+</template>
+
+<style scoped></style>
