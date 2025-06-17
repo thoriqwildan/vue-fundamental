e12ca9576a11ee0406e366e6b312478115d266d4
thoriqwildan <wildanthoriq14@gmail.com>
2025-06-17 09:13:43 +0700

    Sampai JS expression

---
+++ vue-dasar/src/components/HelloVue.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+console.info("Hello vue component loaded");
+
+const hello = "<h1>Hello Thoriq</h1>";
+const classHello = "hello"; // Template attribute binding
+const buttonDisabled = false; // Boolean attribute binding
+
+// Multiple attribute binding
+const data = {
+  id: "hello",
+  class: "hello",
+};
+</script>
+
+<template>
+  <h1 :class="classHello">{{ hello }}</h1>
+  <div :class="classHello" v-html="hello"></div>
+
+  <h1 v-bind="data">Ini multiple Attribute</h1>
+
+  <button :disabled="buttonDisabled">Play The Game!</button>
+</template>
+
+<style scoped>
+.hello {
+  color: green;
+}
+</style>
--- vue-dasar/vite.config.ts
+++ vue-dasar/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello: "hello.html",
+      },
+    },
+  },
+});
