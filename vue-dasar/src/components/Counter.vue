26e87c9069a12c3a42a57caf7032a74874ae3a42
thoriqwildan <wildanthoriq14@gmail.com>
2025-06-17 09:43:00 +0700

    Learn state (state and reactive state)

---
+++ vue-dasar/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+// let count = 0;
+
+const counter = ref({
+  count: 0,
+  name: "Thoriq",
+});
+
+function increment() {
+  counter.value.count++;
+
+  // State pakai DOM manipulation
+  // document.getElementById("count").innerText = `Counter: ${count}`;
+}
+</script>
+
+<template>
+  <div>
+    <h1 id="count">{{ counter.name }}'s counter: {{ counter.count }}</h1>
+    <button v-on:click="increment">Increment</button>
+  </div>
+</template>
+
+<style scoped></style>
