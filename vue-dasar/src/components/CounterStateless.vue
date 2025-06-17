1209ca901f99d1c698546218971a0fc2addc8d9c
thoriqwildan <wildanthoriq14@gmail.com>
2025-06-17 20:16:54 +0700

    Learn Component Event

---
+++ vue-dasar/src/components/CounterStateless.vue
@@ -0,0 +1,15 @@
+<script setup>
+const { counter, name, increment } = defineProps({
+  counter: Number,
+  name: String,
+  increment: Number,
+});
+const emits = defineEmits(["click"]);
+</script>
+
+<template>
+  <h1>Counter {{ name }} : {{ counter }}</h1>
+  <button @click="emits('click', increment)">Increment {{ name }}</button>
+</template>
+
+<style scoped></style>
--- vue-dasar/src/components/MultipleCounter.vue
+++ vue-dasar/src/components/MultipleCounter.vue
@@ -1,13 +1,32 @@
 <script setup>
+import { ref } from "vue";
 import Counter from "./Counter.vue";
+import CounterStateless from "./CounterStateless.vue";
 
 const names = ["Thoriq", "Tirta"];
+
+const counter = ref(0);
+
+function increment(value) {
+  counter.value += value;
+}
 </script>
 
 <template>
   <Counter name="Thoriq" initial-count="10" />
-
   <Counter name="Tirta" />
+
+  <CounterStateless
+    name="Joko"
+    :counter="counter"
+    @click="increment"
+    :increment="2"
+  />
+  <CounterStateless
+    name="Budi"
+    :counter="counter"
+    @click="() => (counter -= 1)"
+  />
 </template>
 
 <style scoped></style>
