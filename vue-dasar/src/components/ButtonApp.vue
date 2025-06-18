c1efe40484eba00d07add17756f0601026870340
thoriqwildan <wildanthoriq14@gmail.com>
2025-06-18 09:26:22 +0700

    Learn fallthrough Attributes

---
+++ vue-dasar/src/components/ButtonApp.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MyButton from "./MyButton.vue";
+
+function clickHandler() {
+  alert("Button clicked!");
+}
+</script>
+
+<template>
+  <MyButton class="button" @click="clickHandler" name="Thoriq" />
+</template>
+
+<style scoped>
+.button {
+  background-color: blue;
+  color: white;
+  border: none;
+}
+</style>
---
+++ vue-dasar/src/components/MyButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useAttrs } from "vue";
+
+const attributes = useAttrs();
+console.info("Attributes: ", attributes);
+</script>
+
+<template>
+  <button>My Button : {{ attributes.name }}</button>
+</template>
+
+<style scoped></style>
